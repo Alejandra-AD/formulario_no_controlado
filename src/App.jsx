7ec4abc69c3d0963726a7ea505ad323d3ecb6098
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-11-13 16:58:11 -0300

    error class

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NoControlado from "./components/NoControlado";
 
 const App = () => {
   return (
-    <div class="container">
+    <div className="container">
       <h1>Formularios</h1>
       <NoControlado/>
     </div>
--- src/components/NoControlado.jsx
+++ src/components/NoControlado.jsx
@@ -1,23 +1,27 @@
-const NoControlado = () =>{
+const NoControlado = () => {
 
-return(
-    <form>
-        <input type="text" placeholder="Ingrese ToDo" className = "form-control mb-2" name="title"
-        
-        />
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submit");
 
-        <textarea className="form-control mb-2" placeholder="Limpiar caja michis 😹" name="descipcion"/>
-        <select className="form-select mb-2" >
-            <option value="pendiente">Pendiente</option>
-            <option value="completada">Completada</option>
-        </select>
+    };
 
-        <button type="submit" className="form-control btn btn-primary">Guardar Tarea</button>
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Ingrese ToDo" className="form-control mb-2" name="title" />   {/* name para usar el form data */}
+            <textarea className="form-control mb-2" placeholder="Limpiar caja michis 😹" name="descipcion" />
+
+            <select className="form-select mb-2" name="state">
+                <option value="pendiente">Pendiente</option>
+                <option value="completada">Completada</option>
+            </select>
+
+            <button type="submit" className="btn btn-primary ">Guardar Tarea</button>
 
 
-    </form>
-     
-);
+        </form>
+
+    );
 
 };
 
