42f9cfd3f912a40fcbf98606ee5d4e52c7f57aa5
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-11-13 11:11:48 -0300

    Update NoControlado.jsx

--- src/components/NoControlado.jsx
+++ src/components/NoControlado.jsx
@@ -2,17 +2,17 @@ const NoControlado = () =>{
 
 return(
     <form>
-        <input type="text" placeholder="Ingrese ToDo" className = "form-control mb-2" 
+        <input type="text" placeholder="Ingrese ToDo" className = "form-control mb-2" name="title"
         
         />
 
-        <textarea className="form-control mb-2" placeholder="Limpiar caja michis 😹"/>
+        <textarea className="form-control mb-2" placeholder="Limpiar caja michis 😹" name="descipcion"/>
         <select className="form-select mb-2" >
             <option value="pendiente">Pendiente</option>
             <option value="completada">Completada</option>
         </select>
 
-        <button type="submit" className="form-control bg-primary">Guardar Tarea</button>
+        <button type="submit" className="form-control btn btn-primary">Guardar Tarea</button>
 
 
     </form>
